eeaa59f99f07496676da0371b72011ab610425a1
Lars Asplund <lars.anders.asplund@gmail.com>
2020-06-02 22:57:54 +0200

    Test vector with many elements - GREEN

--- src/string_pkg.vhd
+++ src/string_pkg.vhd
@@ -3,9 +3,19 @@ package string_pkg is
 end package;
 
 package body string_pkg is
-   function to_string (data : integer_vector) return string is
+  function to_string (data : integer_vector) return string is
+    function list(data : integer_vector) return string is
+    begin
+      if data'length = 1 then
+        return to_string(data(data'left));
+      else
+        return to_string(data(data'left)) & ", " & list(data(data'left + 1 to data'right));
+      end if;
+    end;
    begin
-     if data'length = 1 then
+     if data'length > 1 then
+       return "(" & list(data) & ")";
+     elsif data'length = 1 then
        return "(" & to_string(data(data'left)) & ")";
      else
        return "()";
