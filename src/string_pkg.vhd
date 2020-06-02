15d89d54976fd76607657e4c8ddb852ca9007dc6
Lars Asplund <lars.anders.asplund@gmail.com>
2020-06-02 23:04:50 +0200

    Test vector with many elements - REFACTOR

--- src/string_pkg.vhd
+++ src/string_pkg.vhd
@@ -6,19 +6,15 @@ package body string_pkg is
   function to_string (data : integer_vector) return string is
     function list(data : integer_vector) return string is
     begin
-      if data'length = 1 then
+      if data'length = 0 then
+        return "";
+      elsif data'length = 1 then
         return to_string(data(data'left));
       else
         return to_string(data(data'left)) & ", " & list(data(data'left + 1 to data'right));
       end if;
     end;
    begin
-     if data'length > 1 then
-       return "(" & list(data) & ")";
-     elsif data'length = 1 then
-       return "(" & to_string(data(data'left)) & ")";
-     else
-       return "()";
-     end if;
-  end function;
+     return "(" & list(data) & ")";
+   end function;
 end package body;
