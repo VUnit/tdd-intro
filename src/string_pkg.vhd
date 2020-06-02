39a99d5aa36bd23e4517903b0dd47b957d0f08a1
Lars Asplund <lars.anders.asplund@gmail.com>
2020-06-02 22:52:05 +0200

    Test vector with single element - GREEN

--- src/string_pkg.vhd
+++ src/string_pkg.vhd
@@ -5,6 +5,10 @@ end package;
 package body string_pkg is
    function to_string (data : integer_vector) return string is
    begin
-    return "()";
+     if data'length = 1 then
+       return "(" & to_string(data(data'left)) & ")";
+     else
+       return "()";
+     end if;
   end function;
 end package body;
