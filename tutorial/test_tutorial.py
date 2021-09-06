124c7e061ab1192257e660d011666afba4ac3e1c
umarcor <unai.martinezcorral@ehu.eus>
2021-09-06 16:23:57 +0200

    tutorial/test: use pathlib instead of join and dirname; pass exercise name to check instead of run file path

--- tutorial/test_tutorial.py
+++ tutorial/test_tutorial.py
@@ -1,12 +1,13 @@
 import unittest
+from sys import executable
 from os import environ
-from os.path import join, dirname
+from pathlib import Path
 from subprocess import call
-import sys
 from tests.common import check_report
-from vunit.sim_if.common import has_simulator, simulator_is, simulator_check
+from vunit.sim_if.common import has_simulator
 
-ROOT = dirname(__file__)
+
+ROOT = Path(__file__).resolve().parent
 
 
 @unittest.skipUnless(has_simulator(), "Requires simulator")
@@ -16,16 +17,16 @@ class TestExternalRunScripts(unittest.TestCase):
     """
 
     def test_exercise_01(self):
-        self.check(join(ROOT, "exercise_01", "solution", "run.py"))
+        self.check("exercise_01")
 
     def test_exercise_02(self):
-        self.check(join(ROOT, "exercise_02", "solution", "run.py"))
+        self.check("exercise_02")
 
     def test_exercise_03(self):
-        self.check(join(ROOT, "exercise_03", "solution", "run.py"))
+        self.check("exercise_03")
 
     def test_exercise_04(self):
-        self.check(join(ROOT, "exercise_04", "solution", "run.py"), exit_code=1)
+        self.check("exercise_04", exit_code=1)
         check_report(
             self.report_file,
             [
@@ -50,32 +51,34 @@ class TestExternalRunScripts(unittest.TestCase):
         )
 
     def test_exercise_05(self):
-        self.check(join(ROOT, "exercise_05", "solution", "run.py"))
+        self.check("exercise_05")
 
     def test_exercise_06(self):
-        self.check(join(ROOT, "exercise_06", "solution", "run.py"))
+        self.check("exercise_06")
 
     def test_exercise_07(self):
-        self.check(join(ROOT, "exercise_07", "solution", "run.py"))
+        self.check("exercise_07")
 
-    def check(self, run_file, args=None, vhdl_standard="2008", exit_code=0):
+    def check(self, exercise, args=None, vhdl_standard="2008", exit_code=0):
         """
         Run external run file and verify exit code
         """
-        args = args if args is not None else []
         new_env = environ.copy()
         new_env["VUNIT_VHDL_STANDARD"] = vhdl_standard
-        self.output_path = join(dirname(run_file), "vunit_out")
-        self.report_file = join(self.output_path, "xunit.xml")
-        retcode = call(
-            [
-                sys.executable,
-                run_file,
-                "--clean",
-                "--output-path=%s" % self.output_path,
-                "--xunit-xml=%s" % self.report_file,
-            ]
-            + args,
-            env=new_env,
+        run_file = ROOT / exercise / "solution/run.py"
+        self.output_path = run_file.parent / "vunit_out"
+        self.report_file = self.output_path / "xunit.xml"
+        self.assertEqual(
+            call(
+                [
+                    executable,
+                    run_file,
+                    "--clean",
+                    "--output-path=%s" % self.output_path,
+                    "--xunit-xml=%s" % self.report_file,
+                ]
+                + (args if args is not None else []),
+                env=new_env,
+            ),
+            exit_code,
         )
-        self.assertEqual(retcode, exit_code)
