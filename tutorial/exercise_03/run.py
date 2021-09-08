9e8e290d12b5447bb3c0d0bc1e35462a578b4bf1
umarcor <unai.martinezcorral@ehu.eus>
2021-09-08 03:56:20 +0200

    tutorial/exercise_03: fix simulator conditions

--- tutorial/exercise_03/run.py
+++ tutorial/exercise_03/run.py
@@ -9,7 +9,10 @@ PRJ = VUnit.from_argv()
 ROOT = Path(__file__).resolve().parent
 
 # Add external VHDL libraries to project
-PRJ.add_external_library("lib", ROOT.parent / "exercise_01" / "vunit_out" / "modelsim" / "libraries" / "lib")
+for simulator in ["modelsim", "ghdl", "rivierapro", "activehdl"]:
+    if simulator_is(simulator):
+        PRJ.add_external_library("lib", ROOT.parent.parent / "exercise_01/solution/vunit_out" / simulator / "libraries/lib")
+        break
 
 # Create and add a regular library and add all VHDL files to it
 TB_LIB = PRJ.add_library("tb_lib")
@@ -17,7 +20,8 @@ TB_LIB.add_source_files(ROOT / "test" / "*.vhd")
 # or PRJ.add_library("tb_lib").add_source_files(join(root, "test", "*.vhd"))
 
 # Set simulator specific compile options
-PRJ.set_compile_option("rivierapro.vcom_flags", ["-dbg"])
+if simulator_is("rivierapro"):
+    PRJ.set_compile_option("rivierapro.vcom_flags", ["-dbg"])
 
 # Set generic for all testbenches and test cases
 #PRJ.set_generic("width", 16);
